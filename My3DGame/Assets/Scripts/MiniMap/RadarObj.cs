85b2a8448cbce6459d6f036403cfec5e3c9834ba
ArtomStatsenko <namebyuser@gmail.com>
2020-03-17 00:13:05 +0300

    Добавил радар и миникарту с урока.
    Добавил возможность сохранять данные с урока.

---
+++ My3DGame/Assets/Scripts/MiniMap/RadarObj.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace StatsenkoAA
+{
+	public class RadarObj : MonoBehaviour
+	{
+		[SerializeField] private Image _ico;
+
+        private void OnValidate()
+        {
+            _ico = Resources.Load<Image>("UI/Image (1)");
+        }
+
+        private void OnDisable()
+		{
+			Radar.RemoveRadarObject(gameObject);
+		}
+
+		private void OnEnable()
+		{
+			Radar.RegisterRadarObject(gameObject, _ico);
+		}
+	}
+}
